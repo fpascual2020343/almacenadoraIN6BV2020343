35f0e7e3e7b99dec1ed2066e858bd296ee6f7e4b
Franshesco Emmanuel Pascual Ramirez <88805663+fpascual2020343@users.noreply.github.com>
2022-05-08 22:11:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const cors = require('cors');
 var app = express();
 
 const UsuarioRutas = require('./src/routes/usuario.routes');
-const LigaRutas = require('./src/routes/liga.routes')
-const Torneoroutes = require('./src/routes/torneo.routes')
-const ResultadosRoutes = require('./src/routes/resultado.routes')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(cors());
 
-app.use('/api', UsuarioRutas, LigaRutas, Torneoroutes, ResultadosRoutes);
+app.use('/api', UsuarioRutas);
 module.exports = app;
